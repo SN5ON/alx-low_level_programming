700e142751e82d7c19c477896da89e519284067a
SN50N <shalomnyalia@gmail.com>
2023-02-16 15:51:28 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
  * main - Prints out using the puts function
- * Return:0 (Success)
+ * Return: 0 (Success)
  */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
-return(0);
+return (0);
 }
