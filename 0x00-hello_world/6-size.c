fcae787703c551a4cf7dbc48160ab08ccea6c67a
SN50N <shalomnyalia@gmail.com>
2023-02-17 02:44:08 -0800

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
  * main - Entry point
- * Return: Always 0 (Success)
+ * Return: always 0 (success)
  */
 int main(void)
 {
@@ -12,4 +12,3 @@ printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
 printf("Size of a float: %lu byte(s)", sizeof(float));
 return (0);
 }
-
