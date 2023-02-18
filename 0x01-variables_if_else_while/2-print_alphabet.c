e22216072e9a6f7f12ae6f55a80c305a4d25db43
SN50N <shalomnyalia@gmail.com>
2023-02-18 09:12:37 -0800

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - prints the alphabet in lowercase
+ * Returns: always 0
+ */
+int main(void)
+{
+char lc;
+for (lc = 'a'; lc <= 'z'; lc++)
+{
+putchar(lc);
+}
+putchar('\n');
+
+return (0);
+
+}
