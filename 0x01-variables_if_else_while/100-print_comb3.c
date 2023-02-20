355433361e4e578941d83c9fece71828197111e8
SN50N <shalomnyalia@gmail.com>
2023-02-20 15:27:50 -0800

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * main  main block
+ * main - main block
  * Numbers must be separated by ,, followed by a space
  * The two digits must be different
  * 01 and 10 are considered the same combination of the two digits 0 and 1
  * Print only the smallest combination of two digits
  * Numbers should be printed in ascending order, with two digits
- * You can only use the putchar function (every other function (printf, puts, etc…) is forbidden)
+ * You can only use the putchar function
  * You can only use putchar five times maximum in your code
  * You are not allowed to use any variable of type char
  * All your code should be in the main function
@@ -17,7 +17,8 @@ int main(void)
 {
 int c;
 int d = 0;
-while (d < 10)
+
+	while (d < 10)
 	{
 		c = 0;
 		while (c < 10)
@@ -26,7 +27,6 @@ while (d < 10)
 			{
 				putchar('0' + d);
 				putchar('0' + c);
-				
 				if (c + d != 17)
 				{
 					putchar(',');
