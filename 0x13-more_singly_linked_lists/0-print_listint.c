62ebd4fb2c2e4f0f85e4a69c2ab8927c7f2dd262
SN5ON <shalomnyalila@gmail.com>
2023-04-05 03:20:39 -0700

    0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,9 +1,9 @@
 #include "lists.h"
 
 /**
- *main - function that prints all the elements of a listint_t list
- *@h: linked list of type listint_t to print
- * Return: the number of nodes
+ * print_listint - function prints all the elements of a linked list
+ * @h: linked list of type listint_t to print
+ * Return: number of nodes
  */
 size_t print_listint(const listint_t *h)
 {
