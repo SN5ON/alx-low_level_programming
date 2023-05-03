4b66967a49a196eaf973a3aefe578e939ebc4b64
SN5ON <shalomnyalila@gmail.com>
2023-05-02 18:27:45 -0700

    7-get_nodeint.c

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -12,9 +12,9 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	unsigned int n;
 
 	for (n = 0; n < index; n++)
-       {
-	       if (head == NULL)
-			return (NULL);
+	{
+	if (head == NULL)
+	return (NULL);
 		head = head->next;
 	}
 
